4f603e5c9d11f5a132342e592374d9a905329d9e
Medeni Aydın <muhammedmedeniaydin@gmail.com>
2023-08-31 17:35:22 +0300

    crypto crazy with sync, async, concurrent examples completed

---
+++ asyncexamples.py
@@ -0,0 +1,89 @@
+import threading
+import requests
+import time
+import asyncio
+import aiohttp
+
+def get_data_sync(urls):
+    st= time.time()
+    json_array = []
+    for url in urls:
+        json_array.append(requests.get(url).json())
+    et = time.time()
+    elapsed_time = et - st
+    print("Execution time",elapsed_time,"seconds")
+
+
+
+class ThereadingDowloader(threading.Thread):
+    json_array = []
+
+    def __init__(self,url):
+        super().__init__()
+        self.url = url
+
+    def run(self):
+        response = requests.get(self.url)
+        self.json_array.append(response.json())
+        print(self.json_array)
+        return self.json_array
+
+def get_data_threading(urls):
+    st = time.time()
+    threads = []
+    for url in urls:
+        t = ThereadingDowloader(url)
+        t.start()
+        threads.append(t)
+
+    for t in threads:
+        t.join()
+
+    et = time.time()
+    elapsed_time = et - st
+    print("Execution time", elapsed_time, "seconds")
+
+
+async def get_data_async_but_as_wrapper(urls):
+    st = time.time()
+    json_array = []
+    async with aiohttp.ClientSession() as session:
+        for url in urls:
+            async with session.get(url) as response:
+                json_array.append(await response.json())
+
+    et = time.time()
+    elapsed_time = et - st
+    print("Execution time", elapsed_time, "seconds")
+    return json_array
+
+
+async def get_data(session,url,json_array):
+    async with session.get(url) as resp:
+        json_array.append(await resp.json())
+
+
+async def get_data_async_concurrently(urls):
+    st = time.time()
+    json_array = []
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+        for url in urls:
+            tasks.append(asyncio.ensure_future(get_data(session,url,json_array)))
+        await asyncio.gather(*tasks)
+
+
+    et = time.time()
+    elapsed_time = et - st
+    print("Execution time", elapsed_time, "seconds")
+    return json_array
+
+
+
+
+urls = ["https://postman-echo.com/delay/3"] * 10
+# get_data_sync(urls) # 37 seconds
+#get_data_threading(urls) # 4 seconds
+asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+#asyncio.run(get_data_async_but_as_wrapper(urls)) # 34 seconds
+#asyncio.run(get_data_async_concurrently(urls)) # 4 seconds
